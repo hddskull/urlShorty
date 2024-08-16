66ac89413dd8c436023ca2b5dbe1cded44685c32
Max Gladlov <gladkovmd@google.com>
2024-08-16 15:53:34 +0300

    first increment

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/hddskull/urlShorty/internal/handler"
+)
+
+func start(mux *http.ServeMux) {
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler.RootHandler)
+
+	start(mux)
+}
---
+++ internal/handler/rootHandler.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		handleGet(w, r)
+	} else if r.Method == http.MethodPost {
+		handlePost(w, r)
+	} else {
+		http.Error(w, "invalid method", http.StatusBadRequest)
+		return
+	}
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	fmt.Println(id)
+
+	url, err := getURL(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Add("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	w.Write([]byte(url))
+}
+
+func getURL(id string) (string, error) {
+	if id == "EwHXdJfB" {
+		return "https://practicum.yandex.ru/", nil
+	}
+
+	return "", fmt.Errorf("no url found from id: %s", id)
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	bodyB, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	bodyS := string(bodyB)
+	processUrl(bodyS)
+
+	w.Header().Add("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("success"))
+}
+
+func processUrl(b string) {
+
+	fmt.Println(b)
+
+}
