93afb1034a4d7ab3174e5f4d7c27031eeaadd01f
Max Gladlov <gladkovmd@google.com>
2024-08-30 16:18:43 +0300

    new routing with chi

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,14 +3,19 @@ package app
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/hddskull/urlShorty/internal/handler"
 )
 
 func Start() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler.RootHandler)
+	r := chi.NewRouter()
 
-	err := http.ListenAndServe(":8080", mux)
+	r.Route("/", func(r chi.Router) {
+		r.Post("/", handler.RootPostHandler)
+		r.Get("/{id}", handler.RootGetHandler)
+	})
+
+	err := http.ListenAndServe(":8080", r)
 
 	if err != nil {
 		panic(err)
