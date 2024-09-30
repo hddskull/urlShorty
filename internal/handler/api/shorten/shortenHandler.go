ccd8c62b62d411500fbb621843d1589cef831330
Max Gladlov <gladkovmd@google.com>
2024-09-30 15:11:30 +0300

    handler and test

--- internal/handler/api/shorten/shortenHandler.go
+++ internal/handler/api/shorten/shortenHandler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hddskull/urlShorty/config"
 	"github.com/hddskull/urlShorty/internal/storage"
 	"github.com/hddskull/urlShorty/internal/utils"
+	"github.com/hddskull/urlShorty/tools/custom"
 	"net/http"
 )
 
@@ -19,18 +20,21 @@ type (
 )
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
 
 	reqModel := requestPostModel{}
 	if err := json.NewDecoder(r.Body).Decode(&reqModel); err != nil {
 		utils.SugaredLogger.Debugln("PostHandler decoding error:", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		formattedError := custom.ErrorResponseModel{err.Error()}
+		custom.JSONError(w, formattedError, http.StatusBadRequest)
 		return
 	}
 
 	id, err := storage.TempStorage.Save(reqModel.URL)
 	if err != nil {
 		utils.SugaredLogger.Debugln("PostHandler saving to storage error:", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		formattedError := custom.ErrorResponseModel{err.Error()}
+		custom.JSONError(w, formattedError, http.StatusBadRequest)
 		return
 	}
 
@@ -40,13 +44,13 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 		Result: fullID,
 	}
 
-	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
 	err = json.NewEncoder(w).Encode(resModel)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
+		utils.SugaredLogger.Debugln("PostHandler encoding error:", err)
+		formattedError := custom.ErrorResponseModel{err.Error()}
+		custom.JSONError(w, formattedError, http.StatusBadRequest)
 		return
 	}
 }
---
+++ internal/handler/api/shorten/shortenHandler_test.go
@@ -0,0 +1,113 @@
+package shorten
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+const (
+	localhost = "http://localhost:8080/"
+)
+
+// Post success
+// Post fail - empty model
+// Post fail - wrong model
+// Post fail - plain/text
+func TestPostHandler(t *testing.T) {
+	type want struct {
+		contentType string
+		code        int
+	}
+
+	type testCase struct {
+		name         string
+		method       string
+		url          string
+		contentType  string
+		requestModel interface{} //requestPostModel
+		want         want
+	}
+
+	type wrongModel struct {
+		Text string `json:"text"`
+	}
+
+	tests := []testCase{
+		{
+			name:        "Post success",
+			method:      http.MethodPost,
+			url:         localhost,
+			contentType: "application/json",
+			requestModel: requestPostModel{
+				URL: "https://yandex.ru/",
+			},
+			want: want{
+				contentType: "application/json",
+				code:        http.StatusCreated,
+			},
+		},
+		{
+			name:         "Post fail empty model",
+			method:       http.MethodPost,
+			url:          localhost,
+			contentType:  "application/json",
+			requestModel: requestPostModel{},
+			want: want{
+				contentType: "application/json",
+				code:        http.StatusBadRequest,
+			},
+		},
+		{
+			name:        "Post fail wrong model",
+			method:      http.MethodPost,
+			url:         localhost,
+			contentType: "application/json",
+			requestModel: wrongModel{
+				Text: "wrong model, wrong text",
+			},
+			want: want{
+				contentType: "application/json",
+				code:        http.StatusBadRequest,
+			},
+		},
+		{
+			name:         "Post fail plain text",
+			method:       http.MethodPost,
+			url:          localhost,
+			contentType:  "plain/text",
+			requestModel: "https://yandex.ru/",
+			want: want{
+				contentType: "application/json",
+				code:        http.StatusBadRequest,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+
+		t.Run(tc.name, func(t *testing.T) {
+			jsonBytes, _ := json.Marshal(tc.requestModel)
+			jsonString := string(jsonBytes)
+
+			req := httptest.NewRequest(tc.method, tc.url, strings.NewReader(jsonString))
+			req.Header.Set("Content-Type", tc.contentType)
+
+			w := httptest.NewRecorder()
+			var h func(http.ResponseWriter, *http.Request)
+
+			h = PostHandler
+
+			h(w, req)
+
+			result := w.Result()
+			defer result.Body.Close()
+
+			assert.Contains(t, result.Header.Get("Content-Type"), tc.want.contentType)
+			assert.Equal(t, tc.want.code, result.StatusCode)
+		})
+	}
+}
