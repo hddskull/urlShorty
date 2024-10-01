9a55762a1d200aee9d6bd704929006058babe748
Max Gladlov <gladkovmd@google.com>
2024-10-01 16:39:11 +0300

    using chi middleware compressor

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"compress/gzip"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -8,14 +10,15 @@ import (
 	"github.com/hddskull/urlShorty/config"
 	"github.com/hddskull/urlShorty/internal/handler/api/shorten"
 	"github.com/hddskull/urlShorty/internal/handler/root"
-	"github.com/hddskull/urlShorty/internal/middleware"
+	customMiddleware "github.com/hddskull/urlShorty/internal/middleware"
 )
 
 func Start() {
 
 	r := chi.NewRouter()
-	r.Use(middleware.WithLogging)
-	r.Use(middleware.WithGzip)
+	r.Use(middleware.Compress(gzip.DefaultCompression, "application/json", "text/html"))
+	r.Use(customMiddleware.WithLogging)
+	//r.Use(customMiddleware.WithGzip)
 
 	r.Route("/", func(r chi.Router) {
 		r.Post("/", root.RootPostHandler)
