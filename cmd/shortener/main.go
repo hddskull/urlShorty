5420bff70f087abe2e99bbde3d14223c45f735fd
Max Gladlov <gladkovmd@google.com>
2024-11-14 18:47:19 +0300

    debug

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"github.com/hddskull/urlShorty/config"
 	"github.com/hddskull/urlShorty/internal/app"
-	"github.com/hddskull/urlShorty/internal/database"
 	"github.com/hddskull/urlShorty/internal/storage"
 )
 
 func main() {
 	config.Setup()
 	storage.SetupStorage()
-	database.Start()
+	//database.Start()
 	app.Start()
 }
