578298d38842d7287d6a37b9fcf82e47211e2db4
Max Gladlov <gladkovmd@google.com>
2024-12-14 21:00:22 +0300

    debug remove handler

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,7 +3,6 @@ package app
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/hddskull/urlShorty/config"
-	"github.com/hddskull/urlShorty/internal/handler/api/shorten"
 	"github.com/hddskull/urlShorty/internal/handler/api/shorten/batch"
 	"github.com/hddskull/urlShorty/internal/handler/root"
 	customMiddleware "github.com/hddskull/urlShorty/internal/middleware"
@@ -17,12 +16,12 @@ func Start() {
 	r.Use(customMiddleware.CompressResponseGzip)
 
 	r.Route("/", func(r chi.Router) {
-		r.Post("/", root.PostHandler)
+		//r.Post("/", root.PostHandler)
 		r.Get("/{id}", root.GetHandler)
 		r.Get("/ping", root.PingHandler)
 	})
 	r.Route("/api", func(r chi.Router) {
-		r.Post("/shorten", shorten.PostHandler)
+		//r.Post("/shorten", shorten.PostHandler)
 		r.Post("/shorten/batch", batch.BatchHandler)
 	})
 
